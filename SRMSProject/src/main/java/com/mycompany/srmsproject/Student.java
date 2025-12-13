755e24e42f3ef2de533814ded981b2045a9788f0
ArwaN442 <arwa.xxj@icloud.com>
2025-12-13 10:33:32 +0300

    Student.java

--- SRMSProject/src/main/java/com/mycompany/srmsproject/Student.java
+++ SRMSProject/src/main/java/com/mycompany/srmsproject/Student.java
@@ -10,7 +10,6 @@ public class Student extends Person {
     private double gpa;
     private int year;
 
-    // Constructor
     public Student(String name, int age, String studentId, String major, double gpa, int year) {
         super(name, age); 
         this.studentId = studentId;
@@ -19,7 +18,6 @@ public class Student extends Person {
         this.year = year;
     }
 
-    // Getter & Setter 
     public String getStudentId() {
         return studentId;
     }

71a0be16ff2a5760af1a64952d255425499b9d5f
DELL <DELL@Abrar>
2025-12-16 02:52:57 +0300

    Added CustomException Class

---
+++ SRMSProject/src/main/java/com/mycompany/srmsproject/CustomException.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.srmsproject;
+
+
+public class CustomException extends Exception {
+    public CustomException(String message) {
+        super(message);
+    }
+}
